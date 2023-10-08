34df5e6cd3c0a5daf481b2648bb435be83460938
Javier Valenzuela <javiervalenzuelacontacto@gmail.com>
2023-10-08 15:31:25 -0600

    second commit

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,10 +1,49 @@
 "use client";
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Postgress</li>
+        <li>Express</li>
+        <li>Sequelize</li>
+        <li>JavaScript</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul>
+        <li>dsfsfs</li>
+        <li>sfsfdsf</li>
+        <li>sfsfsf</li>
+        <li>fsdfsfs</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul>
+        <li>bbbbbbbbbb</li>
+        <li>cccccccc</li>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
-  const [tab, setTab] = useState("Skills");
-  const [startTransition, isPending] = useTransition();
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
 
   const handleTabChange = (id) => {
     startTransition(() => {
@@ -15,7 +54,7 @@ const AboutSection = () => {
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/about-image.png" width={500} height={500} />
-        <div>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
@@ -29,12 +68,28 @@ const AboutSection = () => {
             voluptatibus veritatis aliquid repellat quisquam enim iure, quos
             minus nobis voluptates qui incidunt esse!
           </p>
-          <div className="flex flex-row mt-8">
-            <span className="mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500">
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
               Skills
-            </span>
-            <span>Education</span>
-            <span>Experience</span>
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
       </div>
